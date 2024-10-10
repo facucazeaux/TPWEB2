31bc659e53dc0392f263bbbb97be6650a1f268fc
facucazeaux <144554737+facucazeaux@users.noreply.github.com>
2024-10-10 19:05:22 -0300

    hago cambios en el css

--- router.php
+++ router.php
@@ -1,41 +1,42 @@
 <?php
 
-    require_once 'controller/controller.series.php';
-    require_once 'view/view.series.php';
-    require_once 'model/model.series.php';
-    
+require_once 'controller/controller.series.php';
+require_once 'view/view.series.php';
+require_once 'model/model.series.php';
 
+$controller = new controller_series();
 
+$action = !empty($_GET["action"]) ? $_GET["action"] : '';
 
-    $controller = new controller_series();
+$params = explode("/", $action);
 
-    $action = !empty($_GET["action"]) ? $_GET["action"] : '';
+switch ($params[0]) {
+    case 'series':
+        if (isset($params[1]) && $params[1] === 'TodasLasSeries') {
+            $controller->ListarTodasLasSeries();
+        } elseif ($params[1] === 'ver' && isset($params[2])) {
+            $controller->VerSerie($params[2]);
+        }
+        break;
 
-    $params = explode("/",$action);
-
-    switch ($params[0]) {
-        case 'series':
-            if (isset( $params[1]) && $params[1]==='TodasLasSeries')
-                {
-                    $controller->ListarTodasLasSeries();
-
-                
-                } elseif ($params[1] === 'ver' && isset($params[2])) {
-                    
-                    $controller->VerSerie($params[2]);
+        case 'generos':
+            // Listar todos los géneros
+            if (isset($params[1]) && $params[1] === 'ListaGeneros') {
+                if (isset($params[2])) {
+                    // Si se recibe un id de género en la URL, mostrar las series de ese género
+                    $id_genero = $params[2];
+                    $controller->SeriesPorGenero($id_genero);
+                } else {
+                    // Si no se recibe un id de género, mostrar la lista de todos los géneros
+                    $controller->ListarTodosLosGeneros();
                 }
-            
+            }
             break;
-
         
+
+    default:
         
-        default:
-            
-            break;
-    }
-    
-
-
-
-
+        echo "Página no encontrada.";
+        break;
+}
 ?>
